dd2b2f1857beeb5310a532c11ca95066fdeb3dbe
MirkoKiwi <mirko.cenedese@outlook.com>
2025-06-03 17:51:50 +0200

    Added DELETE - Events endpoint

--- app/routers/events.py
+++ app/routers/events.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from app.config import config
 
-from sqlmodel import Session, select
+from sqlmodel import Session, select, delete
 
 from typing import List
 from app.models.event import Event, EventCreate
@@ -20,6 +20,9 @@ templates = Jinja2Templates(directory=config.root_dir / "templates")
 # GET - Events
 @router.get("/", response_model=List[Event])
 async def get_events(session: SessionDep):
+    '''
+    List all events
+    '''
     events = session.exec(select(Event)).all()
     return events
 
@@ -27,9 +30,21 @@ async def get_events(session: SessionDep):
 # POST - Events
 @router.post("/", response_model=Event)
 async def create_event(session: SessionDep, event: EventCreate):
+    '''
+    Create a new event
+    '''
     new_event = Event(**event.dict())
     session.add(new_event)
     session.commit()
     session.refresh(new_event)
     return new_event
 
+
+# DELETE - Events
+@router.delete("/", status_code=204)
+async def delete_all_events(session: SessionDep):
+    '''
+    Delete all events (Irreversible!!!)
+    '''
+    session.exec(delete(Event))  # SQL for bulk deletion
+    session.commit()
