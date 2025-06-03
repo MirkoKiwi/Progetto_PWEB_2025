6e48b0b5687bc39b448a28341572d4e0a1a9888b
MirkoKiwi <mirko.cenedese@outlook.com>
2025-06-03 18:04:19 +0200

    Added DELETE - Events endpoint

--- app/routers/events.py
+++ app/routers/events.py
@@ -21,7 +21,7 @@ templates = Jinja2Templates(directory=config.root_dir / "templates")
 @router.get("/", response_model=List[Event])
 async def get_events(session: SessionDep):
     '''
-    List all events
+    Returns the list of existing events.
     '''
     events = session.exec(select(Event)).all()
     return events
@@ -31,7 +31,7 @@ async def get_events(session: SessionDep):
 @router.post("/", response_model=Event)
 async def create_event(session: SessionDep, event: EventCreate):
     '''
-    Create a new event
+    Creates a new event.
     '''
     new_event = Event(**event.dict())
     session.add(new_event)
@@ -44,7 +44,19 @@ async def create_event(session: SessionDep, event: EventCreate):
 @router.delete("/", status_code=204)
 async def delete_all_events(session: SessionDep):
     '''
-    Delete all events (Irreversible!!!)
+    Delete all events (Irreversible!!!).
     '''
-    session.exec(delete(Event))  # SQL for bulk deletion
+    session.exec(delete(Event)) 
     session.commit()
+
+
+# GET /events/{id}
+@router.get("/{event_id}", response_model=Event)
+async def get_event_by_id(session: SessionDep, event_id: int, title="The ID of the event to retrieve"):
+    '''
+    Returns the event with the given id.
+    '''
+    event = session.get(Event, event_id)
+    if not event:
+        raise HTTPException(status_code=404, detail=f"Event with id {event_id} not found")
+    return event
